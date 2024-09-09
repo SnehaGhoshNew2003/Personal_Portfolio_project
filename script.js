07d3e304ab3dd1d90e13bf191aaee9987fd9cb7d
SnehaGhosh2003 <snehaghosh731@gmail.com>
2024-09-10 04:18:42 +0530

    Uploading Required Files
    index.html
    ML_Sneha_Ghosh.pdf
    Profile_Picture.png
    script.js
    style.css

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.circlechart').each(function() {
+        var percentage = $(this).data('percentage');
+        $(this).circleProgress({
+            value: percentage / 100,
+            size: 200,
+            thickness: 10,
+            fill: {
+                color: '#007ced'
+            },
+            emptyFill: '#333',
+            animation: {
+                duration: 1500
+            }
+        }).on('circle-animation-progress', function(event, progress, stepValue) {
+            $(this).find('strong').text(Math.round(stepValue * 100) + '%');
+        });
+    });
+});
